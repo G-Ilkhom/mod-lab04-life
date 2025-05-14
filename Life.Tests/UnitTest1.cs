4d2cc577c93c91a42e3c028097029fa80c8abcd3
ILKHOM <gaffarovilkhom@inbox.ru>
2025-05-14 20:58:46 +0300

    Remake 2

--- Life.Tests/UnitTest1.cs
+++ Life.Tests/UnitTest1.cs
@@ -16,7 +16,7 @@ namespace Life.Tests
             int height = b.Grid.GetLength(1);
             for (int x = 0; x < width; x++)
                 for (int y = 0; y < height; y++)
-                    if (b.Grid[x, y].IsAlive)
+                    if (b.Grid[x, y].FluxState)
                         alive.Add((x, y));
             return alive;
         }
@@ -32,7 +32,7 @@ namespace Life.Tests
         public void DeadBoard_RemainsEmpty()
         {
             var b = CreateBoard(5, 5);
-            b.Advance();
+            b.ProgressFrame();
             Assert.Empty(GetAlive(b));
         }
 
@@ -40,8 +40,8 @@ namespace Life.Tests
         public void SingleCell_DiesNextGeneration()
         {
             var b = CreateBoard(5, 5);
-            b.Grid[2, 2].IsAlive = true;
-            b.Advance();
+            b.Grid[2, 2].FluxState = true;
+            b.ProgressFrame();
             Assert.Empty(GetAlive(b));
         }
 
@@ -49,9 +49,9 @@ namespace Life.Tests
         public void PairCells_BothDie()
         {
             var b = CreateBoard(5, 5);
-            b.Grid[1, 1].IsAlive = true;
-            b.Grid[1, 2].IsAlive = true;
-            b.Advance();
+            b.Grid[1, 1].FluxState = true;
+            b.Grid[1, 2].FluxState = true;
+            b.ProgressFrame();
             Assert.Empty(GetAlive(b));
         }
 
@@ -61,21 +61,21 @@ namespace Life.Tests
             var b = CreateBoard(5, 5);
             var init = new[] { (1, 2), (2, 2), (3, 2) };
             foreach (var (x, y) in init)
-                b.Grid[x, y].IsAlive = true;
-            b.Advance();
+                b.Grid[x, y].FluxState = true;
+            b.ProgressFrame();
             var expectedVert = new[] { (2, 1), (2, 2), (2, 3) };
             AssertCellsEqual(expectedVert, GetAlive(b));
         }
 
         [Fact]
-        public void ThreeVertical_Periodic_ReturnsToInitialAfterTwoAdvances()
+        public void ThreeVertical_Periodic_ReturnsToInitialAfterTwoFrames()
         {
             var b = CreateBoard(5, 5);
             var init = new[] { (2, 1), (2, 2), (2, 3) };
             foreach (var (x, y) in init)
-                b.Grid[x, y].IsAlive = true;
-            b.Advance();
-            b.Advance();
+                b.Grid[x, y].FluxState = true;
+            b.ProgressFrame();
+            b.ProgressFrame();
             AssertCellsEqual(init, GetAlive(b));
         }
 
@@ -84,8 +84,8 @@ namespace Life.Tests
         {
             var b = CreateBoard(4, 4);
             var init = new[] { (1, 1), (1, 2), (2, 1), (2, 2) };
-            foreach (var c in init) b.Grid[c.Item1, c.Item2].IsAlive = true;
-            b.Advance();
+            foreach (var c in init) b.Grid[c.Item1, c.Item2].FluxState = true;
+            b.ProgressFrame();
             AssertCellsEqual(init, GetAlive(b));
         }
 
@@ -94,8 +94,8 @@ namespace Life.Tests
         {
             var b = CreateBoard(5, 5);
             var init = new[] { (1, 1), (2, 1), (1, 2), (3, 2), (2, 3) };
-            foreach (var c in init) b.Grid[c.Item1, c.Item2].IsAlive = true;
-            b.Advance();
+            foreach (var c in init) b.Grid[c.Item1, c.Item2].FluxState = true;
+            b.ProgressFrame();
             AssertCellsEqual(init, GetAlive(b));
         }
 
@@ -104,8 +104,8 @@ namespace Life.Tests
         {
             var b = CreateBoard(5, 5);
             var init = new[] { (2, 1), (1, 2), (3, 2), (2, 3) };
-            foreach (var c in init) b.Grid[c.Item1, c.Item2].IsAlive = true;
-            b.Advance();
+            foreach (var c in init) b.Grid[c.Item1, c.Item2].FluxState = true;
+            b.ProgressFrame();
             AssertCellsEqual(init, GetAlive(b));
         }
 
@@ -114,18 +114,18 @@ namespace Life.Tests
         {
             var b = CreateBoard(6, 6);
             var init = new[] { (2, 1), (3, 1), (1, 2), (4, 2), (2, 3), (3, 3) };
-            foreach (var c in init) b.Grid[c.Item1, c.Item2].IsAlive = true;
-            b.Advance();
+            foreach (var c in init) b.Grid[c.Item1, c.Item2].FluxState = true;
+            b.ProgressFrame();
             AssertCellsEqual(init, GetAlive(b));
         }
 
         [Fact]
-        public void Glider_MovesDiagonallyAfterFourGenerations()
+        public void Glider_MovesDiagonallyAfterFourFrames()
         {
             var b = CreateBoard(5, 5);
             var glider = new[] { (1, 0), (2, 1), (0, 2), (1, 2), (2, 2) };
-            foreach (var c in glider) b.Grid[c.Item1, c.Item2].IsAlive = true;
-            for (int i = 0; i < 4; i++) b.Advance();
+            foreach (var c in glider) b.Grid[c.Item1, c.Item2].FluxState = true;
+            for (int i = 0; i < 4; i++) b.ProgressFrame();
             var expected = new[] { (2, 1), (3, 2), (1, 3), (2, 3), (3, 3) };
             AssertCellsEqual(expected, GetAlive(b));
         }
@@ -134,9 +134,9 @@ namespace Life.Tests
         public void WrapAround_DetectsNeighborsAcrossEdges()
         {
             var b = CreateBoard(3, 3);
-            b.Grid[0, 0].IsAlive = true;
-            b.Grid[2, 0].IsAlive = true;
-            b.Advance();
+            b.Grid[0, 0].FluxState = true;
+            b.Grid[2, 0].FluxState = true;
+            b.ProgressFrame();
             Assert.Empty(GetAlive(b));
         }
 
@@ -146,7 +146,7 @@ namespace Life.Tests
             var b = CreateBoard(3, 3);
             for (int x = 0; x < b.Grid.GetLength(0); x++)
                 for (int y = 0; y < b.Grid.GetLength(1); y++)
-                    b.Grid[x, y].IsAlive = true;
+                    b.Grid[x, y].FluxState = true;
             var (alive, clusters) = b.Analyze();
             Assert.Equal(9, alive);
             Assert.Equal(1, clusters);
@@ -158,7 +158,7 @@ namespace Life.Tests
             var b = CreateBoard(6, 6);
             var clusterA = new[] { (1, 1), (1, 2) };
             var clusterB = new[] { (4, 4), (5, 4), (4, 5) };
-            foreach (var c in clusterA.Concat(clusterB)) b.Grid[c.Item1, c.Item2].IsAlive = true;
+            foreach (var c in clusterA.Concat(clusterB)) b.Grid[c.Item1, c.Item2].FluxState = true;
             var (alive, clusters) = b.Analyze();
             Assert.Equal(5, alive);
             Assert.Equal(2, clusters);
@@ -169,7 +169,7 @@ namespace Life.Tests
         {
             var b = CreateBoard(3, 3);
             var coords = new[] { (0, 0), (0, 1), (0, 2), (2, 2) };
-            foreach (var c in coords) b.Grid[c.Item1, c.Item2].IsAlive = true;
+            foreach (var c in coords) b.Grid[c.Item1, c.Item2].FluxState = true;
             var (_, clusters) = b.Analyze();
             Assert.Equal(1, clusters);
         }
@@ -179,8 +179,8 @@ namespace Life.Tests
         {
             var b = CreateBoard(6, 6);
             var phase1 = new[] { (2, 2), (3, 2), (4, 2), (1, 3), (2, 3), (3, 3) };
-            foreach (var c in phase1) b.Grid[c.Item1, c.Item2].IsAlive = true;
-            b.Advance();
+            foreach (var c in phase1) b.Grid[c.Item1, c.Item2].FluxState = true;
+            b.ProgressFrame();
             var phase2 = new[] { (1, 2), (1, 3), (2, 4), (3, 1), (4, 2), (4, 3) };
             AssertCellsEqual(phase2, GetAlive(b));
         }
@@ -190,8 +190,8 @@ namespace Life.Tests
         {
             var b = CreateBoard(6, 6);
             var phase1 = new[] { (1, 1), (2, 1), (1, 2), (2, 2), (3, 3), (4, 3), (3, 4), (4, 4) };
-            foreach (var c in phase1) b.Grid[c.Item1, c.Item2].IsAlive = true;
-            b.Advance();
+            foreach (var c in phase1) b.Grid[c.Item1, c.Item2].FluxState = true;
+            b.ProgressFrame();
             var phase2 = new[] { (1, 1), (2, 1), (1, 2), (4, 3), (3, 4), (4, 4) };
             AssertCellsEqual(phase2, GetAlive(b));
         }
@@ -201,9 +201,9 @@ namespace Life.Tests
         {
             var b = CreateBoard(5, 5);
             var initial = new[] { (1, 2), (2, 2), (3, 2) };
-            foreach (var c in initial) b.Grid[c.Item1, c.Item2].IsAlive = true;
-            b.Advance();
-            b.Advance();
+            foreach (var c in initial) b.Grid[c.Item1, c.Item2].FluxState = true;
+            b.ProgressFrame();
+            b.ProgressFrame();
             AssertCellsEqual(initial, GetAlive(b));
         }
     }
